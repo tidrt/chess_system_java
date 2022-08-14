411d87e86326d4700fdb58b4824e36e3d3428948
Tiago <tiago@ufrgs.br>
2022-08-14 18:51:18 -0300

    handling execption and cleaning screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,6 +28,11 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -41,7 +46,6 @@ public class UI {
 		
 	}
 	
-
 	public static void printBoard(ChessPiece[][] pieces) {
 		for(int i=0; i<pieces.length; i++) {
 			System.out.print((8-i) + " ");
